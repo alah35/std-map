1fd857cd52df1eda8eaf11f1f1ea0a9a6f398ede
Maxim Akimenko <maxnet663@gmail.com>
2022-05-14 00:13:34 +0300

    Task2 DOne

---
+++ task2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <map>
+#include <string>
+
+int main() {
+    std::multimap<std::string, int> queue;
+    std::multimap<std::string, int>::iterator it;
+    int counter = 1, pointer = 0;
+    std::string input;
+
+    while (true) {
+        std::cout << "Waiting for input..." << std::endl;
+        std::cin >> input;
+
+        if (input == "q" || input == "quit")
+            break;
+
+        if (input == "Next" || input == "next") {
+            if (queue.empty()) {
+                std::cout << "Queue is empty" << std::endl;
+                continue;
+            }
+            it = queue.begin();
+            std::cout << it->first << std::endl;
+            queue.erase(it);
+            continue;
+        }
+
+        queue.insert(std::make_pair(input, counter));
+        counter++;
+    }
+
+    return 0;
+}
